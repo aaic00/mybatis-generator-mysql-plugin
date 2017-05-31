d63d4d2fbe02ca643932a2d9006f09cc793d45a0
aAic <996202@qq.com>
2017-05-31 17:59:46 +0800

    catalog

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,3 @@
 copy org.mybatis.generator.core_1.3.5.201609070108.jar %ECLIPSE_HOME%\plugins\
-jar uf %ECLIPSE_HOME%\plugins\org.mybatis.generator.core_1.3.5.201609070108.jar -C target/classes com
+jar uf %ECLIPSE_HOME%\plugins\org.mybatis.generator.core_1.3.5.201609070108.jar -C target/classes com 
+jar uf %ECLIPSE_HOME%\plugins\org.mybatis.generator.core_1.3.5.201609070108.jar -C target/classes org\mybatis\generator\internal\util
